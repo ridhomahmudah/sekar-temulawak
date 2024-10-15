cc3941ef9a7a2a7800029150a8816cc46714a753
Ranapace565 <Ranapace565@users.noreply.github.com>
2024-10-16 00:01:30 +0700

    Create ubah_artikel.blade.php

---
+++ backend-sekar/resources/views/admin_konten/ubah_artikel.blade.php
@@ -0,0 +1,83 @@
+<x-admin_layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-4xl lg:py-16">
+            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Tambahkan Artikel</h2>
+            <form action="#">
+                <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                    <div class="sm:col-span-2">
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Judul
+                            Artikel</label>
+                        <input type="text" name="name" id="nama_P"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Manfaat temulawak" required="">
+                    </div>
+                    <div>
+                        <label for="category"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kategori</label>
+                        <select id="Ukuran_P"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option selected="">Kesehatan</option>
+                            <option value="TV">Sekar Temulawak</option>
+                            <option value="PC">Manfaat</option>
+                            <option value="GA">Umum</option>
+                        </select>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+                <div class="w-full my-4">
+                    <label for="body" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body
+                        Artikel</label>
+
+                    <input id="body" type="hidden" name="body">
+                    <trix-editor input="body"></trix-editor>
+                </div>
+                <main>
+                    <trix-toolbar id="my_toolbar"></trix-toolbar>
+                    <div class="more-stuff-inbetween"></div>
+
+                    <trix-editor toolbar="my_toolbar" input="my_input"></trix-editor>
+                </main>
+                <div class="col-span-full">
+                    <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Gambar
+                        Artikel</label>
+                    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                        <div class="text-center">
+                            <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor"
+                                aria-hidden="true" data-slot="icon">
+                                <path fill-rule="evenodd"
+                                    d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                            <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                                <label for="file-upload"
+                                    class="relative cursor-pointer rounded-md bg-white font-semibold text-green-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-primary-500 focus-within:ring-offset-2 hover:text-primary-500">
+                                    <span>Upload gambar</span>
+                                    <input id="file-upload" name="file-upload" type="file" class="sr-only">
+                                </label>
+                                <p class="pl-1">atau seret dan jatuhkan</p>
+                            </div>
+                            <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                        </div>
+                    </div>
+                </div>
+                <button type="button"
+                    class="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                    onclick="window.history.back();">
+                    Batal
+                </button>
+                <button type="submit"
+                    class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                    Simpan Perubahan
+                </button>
+            </form>
+        </div>
+    </section>
+
+
+</x-admin_layout>
