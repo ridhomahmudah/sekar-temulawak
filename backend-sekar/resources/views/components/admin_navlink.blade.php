1e2f0108986829160664437d6f136b141ac156f8
Ranapace565 <Ranapace565@users.noreply.github.com>
2024-10-16 14:30:43 +0700

    Create admin_navlink.blade.php

---
+++ backend-sekar/resources/views/components/admin_navlink.blade.php
@@ -0,0 +1,4 @@
+@props(['active' => false])
+<a {{ $attributes }}
+    class="{{ $active ? 'bg-green-600 text-white' : 'text-green-600 hover:bg-green-600 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium "
+    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
