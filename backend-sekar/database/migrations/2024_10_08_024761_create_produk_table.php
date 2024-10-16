7dd37f445e047a0ff21b1ec23c356c4fa2d5e551
Ranapace565 <Ranapace565@users.noreply.github.com>
2024-10-16 18:08:16 +0700

    Update 2024_10_08_024761_create_produk_table.php

--- backend-sekar/database/migrations/2024_10_08_024761_create_produk_table.php
+++ backend-sekar/database/migrations/2024_10_08_024761_create_produk_table.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,8 +15,8 @@ class CreateProdukTable extends Migration
             $table->integer('harga');
             $table->integer('stok');
             $table->string('gambar', 200);
-            $table->integer('status');
-            $table->integer('kategori');
+            // $table->integer('status');
+            // $table->integer('kategori');
             $table->timestamps();
         });
     }
