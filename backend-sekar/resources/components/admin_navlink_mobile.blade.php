b937878a2c367a7ebede1a56eb0935cb0256004a
Ranapace565 <Ranapace565@users.noreply.github.com>
2024-10-16 14:30:08 +0700

    Delete admin_navlink_mobile.blade.php

--- backend-sekar/resources/components/admin_navlink_mobile.blade.php
+++
@@ -1,4 +0,0 @@
-@props(['active' => false])
-<a {{ $attributes }}
-    class="{{ $active ? 'bg-green-600 text-white' : 'text-green-600 hover:bg-green-600 hover:text-white' }} block rounded-md  px-3 py-2 text-sm font-medium "
-    aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
